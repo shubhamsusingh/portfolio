f0c63cd4e3e4d33b64c69e4c716d478b464d0f2e
shubham singh <singhshubham68738@gmail.com>
2025-02-28 01:36:46 +0530

    working on contact page

--- myportfolio/src/components/Contact.jsx
+++ myportfolio/src/components/Contact.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookSquare, faInstagram, faWhatsapp, faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { Row, Col } from 'antd';
+import { Row, Col, Form, Input, Button } from 'antd';
+const { TextArea } = Input;
 
 const Contact = () => {
     return (
@@ -9,7 +10,7 @@ const Contact = () => {
 
             <section className="contact" id="contact">
                 <Row>
-                    <Col span={12}>
+                    <Col span={14} offset={2}>
                         <div className="contact-text">
                             <h2>Contact Me</h2>
                             <h4>Let's Work together</h4>
@@ -34,8 +35,8 @@ const Contact = () => {
                             </div>
                         </div>
                     </Col>
-                    <Col span={12}>
-                        <div className="contact-form" >
+                    <Col span={7} style={{ marginRight: '10px' }}>
+                        {/* <div className="contact-form" >
                             <form action="mailto:singhshubham68738@gmail.com" method="post" encType="text/plain" >
                                 <input type="text" id="name" name="name" placeholder="Enter Your Name" required />
                                 <input type="email" id="email" name="email" placeholder="xxxx2151@gmail.com" required />
@@ -44,7 +45,52 @@ const Contact = () => {
                                 <textarea id="textarea" name="textarea" cols="40" rows="10" placeholder="Enter your Name"></textarea>
                                 <input type="submit" value="submit" className="send" />
                             </form>
-                        </div>
+                        </div> */}
+                        <Row>
+                            <Col span={24}>
+                                <div className="contact-form" >
+                                    {/* <form action="mailto:singhshubham68738@gmail.com" method="post" encType="text/plain" >
+                                        <input type="text" id="name" name="name" placeholder="Enter Your Name" required />
+                                        <input type="email" id="email" name="email" placeholder="xxxx2151@gmail.com" required />
+
+                                        <input type="text" id="subject" name="subject" placeholder="Enter Your subject" />
+                                        <textarea id="textarea" name="textarea" cols="40" rows="10" placeholder="Enter your Name"></textarea>
+                                        <input type="submit" value="submit" className="send" />
+                                    </form> */}
+                                    <Form>
+                                        <Row>
+                                            <Col span={12}>
+                                                <Form.Item>
+                                                    <Input placeholder='Enter Name' />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col span={10} offset={2}>
+                                                <Form.Item>
+                                                    <Input type='email' placeholder='Enter email' />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col span={24}>
+                                                <Form.Item>
+                                                    <Input placeholder='Subject' />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col span={24}>
+                                                <Form.Item>
+                                                    <TextArea rows={4} id="textarea" className='textarea' placeholder='Write Your Intrest' />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col span={16} offset={8}>
+                                                <Form.Item>
+                                                    <Button type="primary" style={{ width: '10vw' }}>Submit</Button>
+                                                </Form.Item>
+                                            </Col>
+
+                                        </Row>
+
+                                    </Form>
+                                </div>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </section>
