91f8d591913afb9b115ba1570ffde39068bc13bc
shubham Singh <singhshubham68738@gmail.com>
2025-02-27 15:36:58 +0530

    working

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
+import About from './components/About'
+import Skill from './components/Skill'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +16,10 @@ function App() {
     <>
       <Header />
       <Home />
+      <About/>
+      <Skill/>
+      <Footer/>
+    
     </>
   )
 }
